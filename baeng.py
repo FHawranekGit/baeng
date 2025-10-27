d07d04ed9cc1e21094fdbae9d8a8ba13e3dc1a3e
FHawranekGit <felix@hawraneks.at>
2025-10-27 16:22:44 +0100

    coarse code structure

---
+++ baeng.py
@@ -0,0 +1,30 @@
+import sys
+import json
+from typing import Literal
+import numpy as np
+from scipy.io import wavfile
+
+
+class ImpulseResponse:
+    def __init__(self, fs: int, duration: int | float):
+        pass
+
+    def __getitem__(self, index: int | float) -> np.float32:
+        pass
+
+    def __setitem__(self, index: int | float, value: float):
+        pass
+
+    def to_numpy(self) -> np.ndarray:
+        pass
+
+
+global_vars = dict()
+
+
+def interpret_codeblock(codeblock: list, local_vars: dict, scope: Literal["global", "local"]):
+    pass
+
+
+if __name__ == "__main__":
+    pass
