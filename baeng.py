802b1117ff095824959199644f1e5524b7bd9d2d
FHawranekGit <felix@hawraneks.at>
2025-11-06 10:37:46 +0100

    work in progress

--- baeng.py
+++ baeng.py
@@ -19,15 +19,50 @@ class ImpulseResponse:
         pass
 
 
-global_vars = dict()
-
-
-def interpret_codeblock(codeblock: list, local_vars: dict, scope: Literal["global", "local"]):
-    pass
-
 class Baeng:
-    def __init__(self, code: list):
-        self.code = code
+    def __init__(self, script: dict):
+        self.script = script
+
+        self.global_vars = dict()
+
+        self.IR = ImpulseResponse(*self.script["IR"])
+
+        self.functions = script.copy()
+        self.functions.pop("IR")
+        self.functions.pop("CODE")
+
+        self.operators = {
+            "if": self._if_op,
+            "while": self._while_op,
+            "define": self._define_op,
+            "setSample": self._set_sample_op,
+            "readSample": self._read_sample_op
+        }
+
+    def _if_op(self):
+        pass
+
+    def _while_op(self):
+        pass
+
+    def _define_op(self):
+        pass
+
+    def _set_sample_op(self):
+        pass
+
+    def _read_sample_op(self):
+        pass
+
+
+    def _interpret_codeblock(self, codeblock: list, local_vars: dict, scope: Literal["global", "local"]):
+        for code_line in codeblock:
+            pass
+
+
+    def run(self):
+        self._interpret_codeblock(self.script["CODE"], local_vars=dict(), scope="global")
+
 
 if __name__ == "__main__":
     pass
