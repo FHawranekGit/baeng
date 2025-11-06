4a92b05762f59bd6b2f05eb290aaea47cd772cd7
FHawranekGit <felix@hawraneks.at>
2025-11-06 12:11:00 +0100

    work in progress

--- baeng.py
+++ baeng.py
@@ -27,6 +27,7 @@ class Baeng:
         self.script = script
 
         self.global_vars = dict()
+        self.local_variables = []
 
         self.IR = ImpulseResponse(self.script["IR"][0], self.script["IR"][1])
 
@@ -49,7 +50,11 @@ class Baeng:
         pass
 
     def _define_op(self, name, value, scope):
-        pass
+        evaluated_value = self._fetch_parameter(value, scope=scope)
+        if scope == "local":
+            self.local_variables[-1].update({name: evaluated_value})
+        elif scope == "global":
+            self.global_vars.update({name: evaluated_value})
 
     def _set_sample_op(self, key, scope):
         pass
@@ -61,6 +66,18 @@ class Baeng:
         out = None
         exec("out = " + string)
         return out
+    
+    def _fetch_parameter(self, obj, scope):
+        if obj is list:
+            return self.operators[obj[0]](*obj[1:], scope=scope)
+        elif obj is str:
+            # local variables
+            obj = self._replace_vars_in_string(obj, self.local_variables[-1])
+            # global vars
+            obj = self._replace_vars_in_string(obj, self.global_vars)
+            return self._eval_string(obj)
+        elif obj is int or obj is float:
+            return obj
 
     def _replace_vars_in_string(self, expr: str, mapping: dict) -> str:
         """assisted by AI"""
@@ -70,9 +87,9 @@ class Baeng:
         # Ersetzungsfunktion: nimmt den gefundenen Namen und ersetzt ihn durch den Wert
         return pattern.sub(lambda m: str(mapping[m.group(0)]), expr)
 
-
-    def _interpret_codeblock(self, codeblock: list, local_vars: dict, scope: Literal["global", "local"]):
+    def _interpret_codeblock(self, codeblock: list, parameters: dict, scope: Literal["global", "local"]):
         print(codeblock)
+        self.local_variables.append(parameters)
         for code_line in codeblock:
             try:
                 self.operators[code_line[0]](*code_line[1:], scope=scope)
@@ -82,29 +99,31 @@ class Baeng:
                         parameter_names = self.user_functions[code_line[0]]["PARAMS"]
                         parameter_values = []
                         for parameter in code_line[1]:
-                            if parameter is list:
-                                parameter_values.append(self.operators[parameter[0]](*parameter[1:], scope=scope))
-                            elif parameter is str:
-                                # local variables
-                                parameter = self._replace_vars_in_string(parameter, local_vars)
-                                #global vars
-                                parameter = self._replace_vars_in_string(parameter, self.global_vars)
-                                parameter_values.append(self._eval_string(parameter))
-                            elif parameter is int or parameter is float:
-                                parameter_values.append(parameter)
+                            #if parameter is list:
+                            #    parameter_values.append(self.operators[parameter[0]](*parameter[1:], scope=scope))
+                            #elif parameter is str:
+                            #    # local variables
+                            #    parameter = self._replace_vars_in_string(parameter, self.local_variables[-1])
+                            #    #global vars
+                            #    parameter = self._replace_vars_in_string(parameter, self.global_vars)
+                            #    parameter_values.append(self._eval_string(parameter))
+                            #elif parameter is int or parameter is float:
+                            #    parameter_values.append(parameter)
+                            parameter_values.append(self._fetch_parameter(parameter, scope=scope))
 
-                        parameters = dict(zip(parameter_names, parameter_values))
+                        new_parameters = dict(zip(parameter_names, parameter_values))
                         self._interpret_codeblock(
                             self.user_functions[code_line[0]]["CODE"],
-                            local_vars=parameters,
+                            parameters=new_parameters,
                             scope="local"
                         )
                 else:
                     raise NotImplementedError()
+        self.local_variables.pop(-1)
 
 
     def run(self):
-        self._interpret_codeblock(self.script["CODE"], local_vars=dict(), scope="global")
+        self._interpret_codeblock(self.script["CODE"], parameters=dict(), scope="global")
 
 
 if __name__ == "__main__":
