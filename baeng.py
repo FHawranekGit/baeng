67cfe5fbe7773c236a0c7d56c676cee4fe615907
ztiromkip <ztirom.kip@gmail.com>
2025-11-13 12:30:30 +0100

    replaced _replace_vars_in_string()
    handled if
    handled local, global, stay local
    handled read/set sample

--- baeng.py
+++ baeng.py
@@ -4,6 +4,8 @@ from typing import Literal
 import numpy as np
 import re
 from scipy.io import wavfile
+from itertools import cycle
+
 
 
 class ImpulseResponse:
@@ -27,9 +29,11 @@ class Baeng:
         self.script = script
 
         self.global_vars = dict()
-        self.local_variables = []
+        self.local_variables = [{}]
 
         self.IR = ImpulseResponse(self.script["IR"][0], self.script["IR"][1])
+        self.SAMPLEPOS = 0
+        # self.possible_sample_positions = cycle(list(range(0, self.IR.fs * self.IR.duration - 1)))
 
         self.user_functions = script.copy()
         self.user_functions.pop("IR")
@@ -40,13 +44,18 @@ class Baeng:
             "while": self._while_op,
             "define": self._define_op,
             "setSample": self._set_sample_op,
-            "readSample": self._read_sample_op
+            "readSample": self._read_sample_op,
         }
 
     def _if_op(self, condition, code_block, scope):
-        pass
+        if scope == "local":
+            scope = "stay_local"
+        if self._fetch_parameter(condition, scope=scope):
+            self._interpret_codeblock(code_block, parameters={}, scope=scope)
 
     def _while_op(self, condition, code_block, scope):
+        if scope == "local":
+            scope = "stay_local"
         pass
 
     def _define_op(self, name, value, scope):
@@ -56,74 +65,70 @@ class Baeng:
         elif scope == "global":
             self.global_vars.update({name: evaluated_value})
 
-    def _set_sample_op(self, key, scope):
-        pass
+    def _set_sample_op(self, key, value, scope):
+        index = self._fetch_parameter(key, scope=scope)
+        self.IR[index] = self._fetch_parameter(value, scope=scope)
 
     def _read_sample_op(self, key, scope) -> float:
-        pass
+        index = self._fetch_parameter(key, scope=scope)
+        value = self.IR[index]
+        return value
 
     def _eval_string(self, string):
-        out = None
-        exec("out = " + string)
+        all_variables = self.local_variables[-1].copy()
+        all_variables.update(self.global_vars)
+        all_variables.update({"SAMPLEPOS": self.SAMPLEPOS, "IR": self.IR})
+        print(all_variables)
+        out = eval(string, {}, all_variables)
         return out
-    
+
     def _fetch_parameter(self, obj, scope):
-        if obj is list:
+        if type(obj) is list:
             return self.operators[obj[0]](*obj[1:], scope=scope)
-        elif obj is str:
-            # local variables
-            obj = self._replace_vars_in_string(obj, self.local_variables[-1])
-            # global vars
-            obj = self._replace_vars_in_string(obj, self.global_vars)
+        elif type(obj) is str:
+            print("is string")
             return self._eval_string(obj)
-        elif obj is int or obj is float:
+        elif type(obj) is int or type(obj) is float:
             return obj
+        else:
+            raise TypeError
 
-    def _replace_vars_in_string(self, expr: str, mapping: dict) -> str:
-        """assisted by AI"""
-        # Regex baut ein Muster aus allen Keys, sortiert nach Länge (wichtig!)
-        pattern = re.compile(r'\b(' + '|'.join(sorted(mapping.keys(), key=len, reverse=True)) + r')\b')
 
-        # Ersetzungsfunktion: nimmt den gefundenen Namen und ersetzt ihn durch den Wert
-        return pattern.sub(lambda m: str(mapping[m.group(0)]), expr)
-
-    def _interpret_codeblock(self, codeblock: list, parameters: dict, scope: Literal["global", "local"]):
+    def _interpret_codeblock(
+        self, codeblock: list, parameters: dict, scope: Literal["global", "local", "stay_local"]
+    ):
         print(codeblock)
-        self.local_variables.append(parameters)
+        if scope == "local":
+            self.local_variables.append(parameters)
         for code_line in codeblock:
             try:
                 self.operators[code_line[0]](*code_line[1:], scope=scope)
             except KeyError:
                 if code_line[0] in self.user_functions:
-                    for SAMPLEPOS in range(self.IR.fs):
+                    for sample_position in range(self.IR.fs * self.IR.duration):
+                        self.SAMPLEPOS = sample_position
                         parameter_names = self.user_functions[code_line[0]]["PARAMS"]
                         parameter_values = []
                         for parameter in code_line[1]:
-                            #if parameter is list:
-                            #    parameter_values.append(self.operators[parameter[0]](*parameter[1:], scope=scope))
-                            #elif parameter is str:
-                            #    # local variables
-                            #    parameter = self._replace_vars_in_string(parameter, self.local_variables[-1])
-                            #    #global vars
-                            #    parameter = self._replace_vars_in_string(parameter, self.global_vars)
-                            #    parameter_values.append(self._eval_string(parameter))
-                            #elif parameter is int or parameter is float:
-                            #    parameter_values.append(parameter)
-                            parameter_values.append(self._fetch_parameter(parameter, scope=scope))
+                            parameter_values.append(
+                                self._fetch_parameter(parameter, scope=scope)
+                            )
 
                         new_parameters = dict(zip(parameter_names, parameter_values))
                         self._interpret_codeblock(
                             self.user_functions[code_line[0]]["CODE"],
                             parameters=new_parameters,
-                            scope="local"
+                            scope="local",
                         )
                 else:
                     raise NotImplementedError()
-        self.local_variables.pop(-1)
-
+        if scope == "local":
+            self.local_variables.pop(-1)
 
     def run(self):
-        self._interpret_codeblock(self.script["CODE"], parameters=dict(), scope="global")
+        self._interpret_codeblock(
+            self.script["CODE"], parameters=dict(), scope="global"
+        )
 
 
 if __name__ == "__main__":
